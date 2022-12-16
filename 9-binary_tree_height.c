2db73f18d37716d1f3ce19c85b1c07e18eb48405
spindouken <spindouken@gmail.com>
2022-12-16 12:40:19 -0600

    big push

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: the number of height or 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t rightTreeHeight, leftTreeHeight;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	leftTreeHeight = binary_tree_height(tree->left);
+	rightTreeHeight = binary_tree_height(tree->right);
+	return (leftTreeHeight > rightTreeHeight ? leftTreeHeight : rightTreeHeight) + 1;
+}
