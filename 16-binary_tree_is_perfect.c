4764854fab2a49a70bb5b8f678db03795ee68133
spindouken <spindouken@gmail.com>
2022-12-16 14:09:28 -0600

    big local push

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * heightFinder - find height of a node
+ * heightFinder - find height for a node
  * @tree: pointer to the root node of the tree to measure the height
  * Return: height or 0 if tree is NULL
  */
@@ -33,4 +33,27 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	return (heightFinder(tree->left) - heightFinder(tree->right));
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 if tree is NULL
+ * return 0 if the tree is not perfect
+ * (the heights of the left and right subtrees are not equal)
+ * returns 1 if tree is perfect
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (binary_tree_balance(tree) != 0)
+		return (0);
+	if ((tree->left) && (tree->right))
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	return (0);
 }
