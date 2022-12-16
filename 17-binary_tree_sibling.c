3d98fba7c493ea83dc8b17f92fccd686dc2b2a07
spindouken <spindouken@gmail.com>
2022-12-16 14:31:46 -0600

    betty compliance

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,11 +10,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    if (node == node->parent->left)
-        return (node->parent->right);
-    else
-        return (node->parent->left);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
