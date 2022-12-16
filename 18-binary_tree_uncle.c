0994c5debc3d6d771ea4c558667ae3f8538e449c
spindouken <spindouken@gmail.com>
2022-12-16 14:28:51 -0600

    betty compliance

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,16 @@
 #include "binary_trees.h"
 
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL)
+        return (NULL);
+
+    if (node == node->parent->left)
+        return (node->parent->right);
+    else
+        return (node->parent->left);
+}
+
 /**
  * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to the node to find the uncle
